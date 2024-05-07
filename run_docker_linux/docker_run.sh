4cf471afb00d569a0b5583b2b67b26d070abab5c
Olamikun <lammyaluko@gmail.com>
2024-05-07 00:14:23 -0600

    updated to run on localnetwork. removed useless commands

--- run_docker_linux/docker_run.sh
+++ run_docker_linux/docker_run.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Stop and remove any existing containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+cd ../
 
-# Run the frontend
-docker run --network weekly-chores-network --name weekly-chores-frontend -p 3000:3000 -d weekly-chores-frontend:1.0
-
-# Run the database
-docker run --network weekly-chores-network --name weekly-chores-database -p 5433:5432 -d weekly-chores-database:1.0
-
-# Run the backend
-docker run --network weekly-chores-network --name weekly-chores-api -p 8000:8000 -d weekly-chores-api:1.0
-
-# Run the database interface
-docker run --network weekly-chores-network --name weekly-chores-database-interface -p 15432:80 -d weekly-chores-database-interface:1.0
+# run docker-compose
+docker-compose up -d
 
 # Open the frontend in the default web browser
 xdg-open http://localhost:3000
--- run_docker_windows/docker_run.sh
+++ run_docker_windows/docker_run.sh
@@ -2,24 +2,8 @@
 
 cd ..
 
-# Stop and remove any existing containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-
-# Create network
-docker network create weekly-chores-network
-
-# Run the frontend
-docker run --network weekly-chores-network --name weekly-chores-frontend -p 3000:3000 -d lammy23/weekly-chores-frontend:1.0
-
-# Run the database
-docker run --network weekly-chores-network --name weekly-chores-database -p 5433:5432 -d lammy23/weekly-chores-database:1.0
-
-# Run the backend
-docker run --network weekly-chores-network --name weekly-chores-api -p 8000:8000 -d lammy23/weekly-chores-api:1.0
-
-# Run the database interface
-docker run --network weekly-chores-network --name weekly-chores-database-interface -p 15432:80 -d lammy23/weekly-chores-database-interface:1.0
+# run docker-compose
+docker-compose up -d
 
 # Open the frontend in the default web browser
 start "" http://localhost:3000
