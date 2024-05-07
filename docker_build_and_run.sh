01d38a2c4eca0e56f8b8dcc869492fed6a259743
Olamikun <lammyaluko@gmail.com>
2024-05-06 21:30:13 -0600

    chore: Update docker_run.sh script to include database interface container

---
+++ docker_build_and_run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Create network
+docker network create weekly-chores-network
+
+# Build and run the frontend
+docker build -t weekly-chores-frontend:1.0 frontend/.
+docker run --network weekly-chores-network --name weekly-chores-frontend -p 3000:3000 -d weekly-chores-frontend:1.0
+
+# Build and run the database
+docker build -t weekly-chores-database:1.0 database/.
+docker run --network weekly-chores-network --name weekly-chores-database -p 5433:5432 -d weekly-chores-database:1.0
+
+# Build and run the backend
+docker build -t weekly-chores-api:1.0 backend/.
+docker run --network weekly-chores-network --name weekly-chores-api -p 8000:8000 -d weekly-chores-api:1.0
+
+# Build and run the database interface
+docker build -t weekly-chores-database-interface:1.0 database_interface/.
+docker run --network weekly-chores-network --name weekly-chores-database-interface -p 15432:80 -d weekly-chores-database-interface:1.0
+
+# Open the frontend in the default web browser
+start "" http://localhost:3000
+start "" http://localhost:15432/login?next=/
+
+# How to run
+# chmod +x docker_build_and_run.sh
+# ./docker_build_and_run.sh
+
+# To tag images (for pushing to Docker Hub)
+docker tag weekly-chores-frontend:1.0 lammy23/weekly-chores-frontend:1.0
+docker tag weekly-chores-database:1.0 lammy23/weekly-chores-database:1.0
+docker tag weekly-chores-api:1.0 lammy23/weekly-chores-api:1.0
+docker tag weekly-chores-database-interface:1.0 lammy23/weekly-chores-database-interface:1.0
+
+# To push images to Docker Hub
+# docker push lammy23/weekly-chores-frontend:1.0
+# docker push lammy23/weekly-chores-database:1.0
+# docker push lammy23/weekly-chores-api:1.0
+# docker push lammy23/weekly-chores-database-interface:1.0
+
--- docker_run.sh
+++ docker_run.sh
@@ -3,20 +3,16 @@
 # Create network
 docker network create weekly-chores-network
 
-# Build and run the frontend
-docker build -t weekly-chores-frontend:1.0 frontend/.
+# Run the frontend
 docker run --network weekly-chores-network --name weekly-chores-frontend -p 3000:3000 -d weekly-chores-frontend:1.0
 
-# Build and run the database
-docker build -t weekly-chores-database:1.0 database/.
+# Run the database
 docker run --network weekly-chores-network --name weekly-chores-database -p 5433:5432 -d weekly-chores-database:1.0
 
-# Build and run the backend
-docker build -t weekly-chores-api:1.0 backend/.
+# Run the backend
 docker run --network weekly-chores-network --name weekly-chores-api -p 8000:8000 -d weekly-chores-api:1.0
 
-# Build and run the database interface
-docker build -t weekly-chores-database-interface:1.0 database_interface/.
+# Run the database interface
 docker run --network weekly-chores-network --name weekly-chores-database-interface -p 15432:80 -d weekly-chores-database-interface:1.0
 
 # Open the frontend in the default web browser
