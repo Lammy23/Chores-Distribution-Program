290edebb4516ebe6b952242f9ab676eb2e3603b4
Lammy23 <lammyaluko@gmail.com>
2024-05-06 21:36:02 -0600

    Changes to docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -4,16 +4,16 @@
 docker network create weekly-chores-network
 
 # Run the frontend
-docker run --network weekly-chores-network --name weekly-chores-frontend -p 3000:3000 -d weekly-chores-frontend:1.0
+docker run --network weekly-chores-network --name weekly-chores-frontend -p 3000:3000 -d lammy23/weekly-chores-frontend:1.0
 
 # Run the database
-docker run --network weekly-chores-network --name weekly-chores-database -p 5433:5432 -d weekly-chores-database:1.0
+docker run --network weekly-chores-network --name weekly-chores-database -p 5433:5432 -d lammy23/weekly-chores-database:1.0
 
 # Run the backend
-docker run --network weekly-chores-network --name weekly-chores-api -p 8000:8000 -d weekly-chores-api:1.0
+docker run --network weekly-chores-network --name weekly-chores-api -p 8000:8000 -d lammy23/weekly-chores-api:1.0
 
 # Run the database interface
-docker run --network weekly-chores-network --name weekly-chores-database-interface -p 15432:80 -d weekly-chores-database-interface:1.0
+docker run --network weekly-chores-network --name weekly-chores-database-interface -p 15432:80 -d lammy23/weekly-chores-database-interface:1.0
 
 # Open the frontend in the default web browser
 start "" http://localhost:3000
